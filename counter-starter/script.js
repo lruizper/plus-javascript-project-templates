45129b67b9af228d24888d00ae558d107808f960
lruizper <lruizper@gmail.com>
2024-02-04 19:08:40 +0800

    Update counter functionality and styles

---
+++ counter-starter/script.js
@@ -0,0 +1,21 @@
+function add(step) {
+    let numberElement = document.getElementById('number');
+    let number = numberElement.innerText;
+    number =parseInt(number)+step;
+    numberElement.value = number;
+    // console.log(number);
+}
+
+function subtract(step) {
+    let numberElement = document.getElementById('number');
+    let number=numberElement.innerText;
+    number=parseInt(number)-step;
+    numberElement.innerText=number;
+    console.log(number);
+    }
+
+function reset() {
+    let numberElement = document.getElementById('number');
+    numberElement.innerText=0;
+    console.log(0);
+    }
