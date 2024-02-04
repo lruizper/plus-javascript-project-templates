bf61cd9b64436f1a1ba1b319ce0d7285618341fa
lruizper <lruizper@gmail.com>
2024-02-04 21:33:13 +0800

    shopping list functionality is active as per class notes

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,34 @@
-function addItem() {}
+let shoppingListItems = ["Milk", "Bread", "Eggs", "Butter", "Cheese"];
+const shoppingList = document.getElementById("shopping-list-items");
 
-function updateItems() {}
+
+function updateItems() {
+    shoppingList.innerHTML = "";
+    for (const item of shoppingListItems) {
+        const listItem = document.createElement("li");
+        listItem.innerText = item;
+        shoppingList.appendChild(listItem);
+    }
+}
+
+let listElement = document.getElementById("shopping-list-items");
+for (const shoppingItem of shoppingListItems) {
+    let itemElement = document.createElement("li");
+    itemElement.innerText = shoppingItem;
+    listElement.appendChild(itemElement);
+    // console.log(shoppingItem);
+}
+
+function addItem() {
+    let input = document.getElementById("new-item-text");
+    const item = input.value;
+    shoppingListItems.push(item);
+    updateItems();
+    input.value = '';
+    // console.log(shoppingListItems);
+}
+
+function clearList() {
+    shoppingListItems = [];
+    updateItems();
+  }
