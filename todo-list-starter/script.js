23dc045648dcdd580eb2182aa37ab91bf63897f7
lruizper <lruizper@gmail.com>
2024-02-08 13:28:34 +0800

    Update todo list with wellbeing tasks and show in the page

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,3 +1,18 @@
+/** creating a to do list of wellbeing tasks */
+let todoTasks = [ "manicure", "use that massage voucher", "go for a walk", "call a friend"];
+// show the tasks in the page
+const todoList = document.getElementById("todo-list");
+
+for (const task of todoTasks) {
+    const newTodoTaskTextElement =document.createElement("p"); // why creating this element?
+    newTodoTaskTextElement.innerText= task;
+    
+    const newTask = document.createElement("li");
+    newTask.innerText = task;
+    todoList.appendChild(newTask);
+}
+
+
 function addTask() {}
 
 function updateTodoList() {}
