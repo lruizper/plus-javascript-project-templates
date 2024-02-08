512d208ee9a97e8c3464de1180cc36c4bb5b3347
lruizper <lruizper@gmail.com>
2024-02-08 14:14:40 +0800

    Added button to each task of the to todo list

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -4,17 +4,24 @@ let todoTasks = [ "manicure", "use that massage voucher", "go for a walk", "call
 const todoList = document.getElementById("todo-list");
 
 /** introducing states */
+// give a status to each task
 let taskStatus=[false,true,false,false]
-
+// iterate through the tasks using their index and create a new list item for each task
 for (const [index, task ] of todoTasks.entries()) {
     const newTask = document.createElement("li");
     newTask.innerText = task;
-
+// if the task is complete, add the class "complete" to the list item
+// this will add the strikethrough effect defined in the CSS file
     if (taskStatus[index]==true) {
-        newTask.classList.add("completed");
+        newTask.classList.add("complete");
     }
-
+// add the list item to the todo list to be shown in the page
     todoList.appendChild(newTask);
+// add a button to mark the task as complete
+    const completeButtonElement = document.createElement("input");
+    completeButtonElement.type = "button";
+    completeButtonElement.value = "Completed";
+    newTask.appendChild(completeButtonElement);
 }
 
 
